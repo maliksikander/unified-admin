cab060c43358cba426b25061b32ac122c2a616c0
saud <=>
2020-12-09 11:02:55 +0500

    adding calendars

--- Frontend/src/app/admin/general-settings/calendar/calendar.component.ts
+++ Frontend/src/app/admin/general-settings/calendar/calendar.component.ts
@@ -1,12 +1,31 @@
-import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, OnInit, TemplateRef, ViewChild } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { MatDialog } from '@angular/material';
 import { CommonService } from '../../services/common.service';
 import { EndpointService } from '../../services/endpoint.service';
 import { SnackbarService } from '../../services/snackbar.service';
+import {
+  startOfDay,
+  endOfDay,
+  subDays,
+  addDays,
+  endOfMonth,
+  isSameDay,
+  isSameMonth,
+  addHours,
+} from 'date-fns';
+import {
+  CalendarEvent,
+  CalendarEventAction,
+  CalendarEventTimesChangedEvent,
+  CalendarView,
+} from 'angular-calendar';
+import { Subject } from 'rxjs';
+// import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-calendar',
+  changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './calendar.component.html',
   styleUrls: ['./calendar.component.scss']
 })
@@ -19,14 +38,106 @@ export class CalendarComponent implements OnInit {
   currentDay;
   calendarForm: FormGroup;
   formHeading = 'Create New Attribute';
-  saveBtnText = 'Save'
+  saveBtnText = 'Save';
+
+  colors: any = {
+    red: {
+      primary: '#ad2121',
+      secondary: '#FAE3E3',
+    },
+    blue: {
+      primary: '#1e90ff',
+      secondary: '#D1E8FF',
+    },
+    yellow: {
+      primary: '#e3bc08',
+      secondary: '#FDF1BA',
+    },
+  };
+  // static: boolean;
+  // @ViewChild('modalContent', { static: true }) modalContent: TemplateRef<any>;
+
+  view: CalendarView = CalendarView.Month;
+
+  CalendarView = CalendarView;
+
+  viewDate: Date = new Date();
+
+  // modalData: {
+  //   action: string;
+  //   event: CalendarEvent;
+  // };
+
+  actions: CalendarEventAction[] = [
+    {
+      label: '<i class="fas fa-fw fa-pencil-alt"></i>',
+      a11yLabel: 'Edit',
+      onClick: ({ event }: { event: CalendarEvent }): void => {
+        this.handleEvent('Edited', event);
+      },
+    },
+    {
+      label: '<i class="fas fa-fw fa-trash-alt"></i>',
+      a11yLabel: 'Delete',
+      onClick: ({ event }: { event: CalendarEvent }): void => {
+        this.events = this.events.filter((iEvent) => iEvent !== event);
+        this.handleEvent('Deleted', event);
+      },
+    },
+  ];
+
+  events: CalendarEvent[] = [
+    // {
+    //   start: subDays(startOfDay(new Date()), 1),
+    //   end: addDays(new Date(), 1),
+    //   title: 'A 3 day event',
+    //   color: this.colors.red,
+    //   actions: this.actions,
+    //   allDay: true,
+    //   resizable: {
+    //     beforeStart: true,
+    //     afterEnd: true,
+    //   },
+    //   draggable: true,
+    // },
+    // {
+    //   start: startOfDay(new Date()),
+    //   title: 'An event with no end date',
+    //   color: this.colors.yellow,
+    //   actions: this.actions,
+    // },
+    // {
+    //   start: subDays(endOfMonth(new Date()), 3),
+    //   end: addDays(endOfMonth(new Date()), 3),
+    //   title: 'A long event that spans 2 months',
+    //   color: this.colors.blue,
+    //   allDay: true,
+    // },
+    // {
+    //   start: addHours(startOfDay(new Date()), 2),
+    //   end: addHours(new Date(), 2),
+    //   title: 'A draggable and resizable event',
+    //   color: this.colors.yellow,
+    //   actions: this.actions,
+    //   resizable: {
+    //     beforeStart: true,
+    //     afterEnd: true,
+    //   },
+    //   draggable: true,
+    // },
+  ];
+
+  activeDayIsOpen: boolean = true;
+  refresh: Subject<any> = new Subject();
 
   constructor(private snackbar: SnackbarService,
     private fb: FormBuilder,
     private commonService: CommonService,
     private endPointService: EndpointService,
     private dialog: MatDialog,
-    private changeDetector: ChangeDetectorRef) { }
+    private changeDetector: ChangeDetectorRef,
+    // private modal: NgbModal
+  ) { }
 
   ngOnInit() {
 
@@ -40,11 +151,11 @@ export class CalendarComponent implements OnInit {
 
 
     // this.licenseForm.valueChanges.subscribe((data) => {
-      //   let result = this.commonService.logValidationErrors(this.amqSettingForm, this.formErrors, this.validations);
-      //   this.formErrors = result[0];
-      //   this.validations = result[1];
-      // });
-  
+    //   let result = this.commonService.logValidationErrors(this.amqSettingForm, this.formErrors, this.validations);
+    //   this.formErrors = result[0];
+    //   this.validations = result[1];
+    // });
+
     this.commonService._spinnerSubject.subscribe((res: any) => {
       this.spinner = res;
       this.changeDetector.markForCheck();
@@ -54,6 +165,11 @@ export class CalendarComponent implements OnInit {
 
   layoutChange(e) {
     this.currentDay = this.dateFormation(e.value);
+
+  }
+
+  monthChange(){
+    console.log("vall-->",this.layout.value);
     
   }
 
@@ -90,7 +206,85 @@ export class CalendarComponent implements OnInit {
     // this.searchTerm = "";
   }
 
-  onSave(){
+  onSave() { }
 
+  dayClicked({ date, events }: { date: Date; events: CalendarEvent[] }): void {
+    if (isSameMonth(date, this.viewDate)) {
+      if (
+        (isSameDay(this.viewDate, date) && this.activeDayIsOpen === true) ||
+        events.length === 0
+      ) {
+        this.activeDayIsOpen = false;
+      } else {
+        this.activeDayIsOpen = true;
+      }
+      this.viewDate = date;
+    }
   }
+
+  eventTimesChanged({
+    event,
+    newStart,
+    newEnd,
+  }: CalendarEventTimesChangedEvent): void {
+    this.events = this.events.map((iEvent) => {
+      if (iEvent === event) {
+        return {
+          ...event,
+          start: newStart,
+          end: newEnd,
+        };
+      }
+      return iEvent;
+    });
+    this.handleEvent('Dropped or resized', event);
+  }
+
+  handleEvent(action: string, event: CalendarEvent): void {
+    // this.modalData = { event, action };
+    // this.modal.open(this.modalContent, { size: 'lg' });
+  }
+
+  addEvent(): void {
+    this.events = [
+      // ...this.events,
+      // {
+      //   title: 'New event',
+      //   start: startOfDay(new Date()),
+      //   end: endOfDay(new Date()),
+      //   color: this.colors.red,
+      //   draggable: true,
+      //   resizable: {
+      //     beforeStart: true,
+      //     afterEnd: true,
+      //   },
+      // },
+    ];
+  }
+
+  deleteEvent(eventToDelete: CalendarEvent) {
+    this.events = this.events.filter((event) => event !== eventToDelete);
+  }
+
+  setView(e) {
+    this.view = e.value;
+    this.currentDay = this.dateFormation(e.value);
+    // console.log("view-->",e)
+  }
+
+  closeOpenMonthViewDay() {
+    this.activeDayIsOpen = false;
+  }
+
 }
+
+
+
+
+
+
+
+
+
+
+
--- build.sh
+++ build.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/env bash -x
-
+version=$1
+if [ -z "$1" ]
+  then
+    echo "Version number is not supplied. Please input version"
+    read version
+fi
 echo 'Build Started at' $(date +'%Y-%m-%d %H:%M:%S.%N')
 # build frontend distribution
 echo 'Building frontend'
+echo 'Version : '$version
 cd frontend
 npm i
 ng build --prod
@@ -15,18 +21,18 @@ branch_name="$(git symbolic-ref HEAD 2>/dev/null)" ||
 branch_name="(unnamed branch)"     # detached HEAD
 finalBranchName=${branch_name##refs/heads/}
 
-if [[ "$finalBranchName" == "master" ]]; then
-  finalBranchName=${finalBranchName:-latest}
-fi
+# if [[ "$finalBranchName" == "master" ]]; then
+  # finalBranchName=${finalBranchName:-latest}
+# fi
 
-image_tag="gitlab.expertflow.com:9242/cim/unified-admin:"${finalBranchName}
+image_tag="gitlab.expertflow.com:9242/cim/unified-admin:"${version}
 
 echo "Going to build image: $image_tag"
 
 # build docker image
 docker build -t $image_tag .
 # push docker image
-# docker push $image_tag .
+docker push gitlab.expertflow.com:9242/cim/unified-admin:$version .
 
 
 
