6c478fb1650b4d58581ed034cc6b02444f18399c
saud <=>
2021-01-21 10:03:57 +0500

    CIM-419

--- Frontend/src/app/admin/general-settings/license-manager/license-manager.component.ts
+++ Frontend/src/app/admin/general-settings/license-manager/license-manager.component.ts
@@ -1,5 +1,5 @@
 import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { CommonService } from '../../services/common.service';
 import { EndpointService } from '../../services/endpoint.service';
 import { SnackbarService } from '../../services/snackbar.service';
@@ -28,7 +28,7 @@ export class LicenseManagerComponent implements OnInit {
 
 
     this.licenseForm = this.fb.group({
-      licenseKey: [''],
+      licenseKey: ['',Validators.required],
     });
 
     // this.licenseForm.valueChanges.subscribe((data) => {
