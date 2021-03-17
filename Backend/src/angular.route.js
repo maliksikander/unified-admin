06d26f2fb123ce072485ff9c636e718608cfc1cd
saud <=>
2021-03-17 10:51:59 +0500

    removing angular routed from app.js

--- Backend/src/angular.route.js
+++ Backend/src/angular.route.js
@@ -1,38 +1,38 @@
-const express = require('express');
-const path = require('path');
-const router = express.Router();
+// const express = require('express');
+// const path = require('path');
+// const router = express.Router();
 
-router.get('/amq-settings', (req, res) => {
-    const indexPath = pathResponse();
-    res.sendFile(indexPath);
-});
-router.get('/database-settings', (req, res) => {
-    const indexPath = pathResponse();
-    res.sendFile(indexPath);
-});
-router.get('/display-settings', (req, res) => {
-    const indexPath = pathResponse();
-    res.sendFile(indexPath);
-});
-router.get('/locale-settings', (req, res) => {
-    const indexPath = pathResponse();
-    res.sendFile(indexPath);
-});
-router.get('/logging-settings', (req, res) => {
-    const indexPath = pathResponse();
-    res.sendFile(indexPath);
-});
-router.get('/reporting-settings', (req, res) => {
-    const indexPath = pathResponse();
-    res.sendFile(indexPath);
-});
-router.get('/security-settings', (req, res) => {
-    const indexPath = pathResponse();
-    res.sendFile(indexPath);
-});
-function pathResponse() {
-    return path.resolve('src', 'public', 'index.html');
-}
+// router.get('/amq-settings', (req, res) => {
+//     const indexPath = pathResponse();
+//     res.sendFile(indexPath);
+// });
+// router.get('/database-settings', (req, res) => {
+//     const indexPath = pathResponse();
+//     res.sendFile(indexPath);
+// });
+// router.get('/display-settings', (req, res) => {
+//     const indexPath = pathResponse();
+//     res.sendFile(indexPath);
+// });
+// router.get('/locale-settings', (req, res) => {
+//     const indexPath = pathResponse();
+//     res.sendFile(indexPath);
+// });
+// router.get('/logging-settings', (req, res) => {
+//     const indexPath = pathResponse();
+//     res.sendFile(indexPath);
+// });
+// router.get('/reporting-settings', (req, res) => {
+//     const indexPath = pathResponse();
+//     res.sendFile(indexPath);
+// });
+// router.get('/security-settings', (req, res) => {
+//     const indexPath = pathResponse();
+//     res.sendFile(indexPath);
+// });
+// function pathResponse() {
+//     return path.resolve('src', 'public', 'index.html');
+// }
 
 
-module.exports = router;
+// module.exports = router;
