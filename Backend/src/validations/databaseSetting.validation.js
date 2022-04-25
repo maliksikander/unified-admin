bac2c6fa51e5902f1b0e9fa4a65df5d2812c0910
saud.iqbal@expertflow.com <saud.iqbal@expertflow.com>
2022-04-25 20:37:03 +0500

    test commit

--- Backend/src/validations/databaseSetting.validation.js
+++ Backend/src/validations/databaseSetting.validation.js
@@ -1,14 +1,15 @@
 const Joi = require('@hapi/joi');
 
+const urlPattern = /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[\-;:&=\+\$,\w]+@)?[A-Za-z0-9\.\-]+|(?:www\.|[\-;:&=\+\$,\w]+@)[A-Za-z0-9\.\-]+)((?:\/[\+~%\/\.\w\-_]*)?\??(?:[\-\+=&;%@\.\w_]*)#?(?:[\.\!\/\\\w]*))?)/;
 const createSetting = {
   body: Joi.object().keys({
-    mongoUrl: Joi.string().pattern(/((([A-Za-z]{3,9}:(?:\/\/)?)(?:[\-;:&=\+\$,\w]+@)?[A-Za-z0-9\.\-]+|(?:www\.|[\-;:&=\+\$,\w]+@)[A-Za-z0-9\.\-]+)((?:\/[\+~%\/\.\w\-_]*)?\??(?:[\-\+=&;%@\.\w_]*)#?(?:[\.\!\/\\\w]*))?)/).required(),
-    eabcDBUrl: Joi.string().pattern(/((([A-Za-z]{3,9}:(?:\/\/)?)(?:[\-;:&=\+\$,\w]+@)?[A-Za-z0-9\.\-]+|(?:www\.|[\-;:&=\+\$,\w]+@)[A-Za-z0-9\.\-]+)((?:\/[\+~%\/\.\w\-_]*)?\??(?:[\-\+=&;%@\.\w_]*)#?(?:[\.\!\/\\\w]*))?)/).required(),
+    mongoUrl: Joi.string().pattern(urlPattern).required(),
+    eabcDBUrl: Joi.string().pattern(urlPattern).required(),
     eabcDBDriver: Joi.string().max(256).required(),
     eabcDBDialect: Joi.string().max(256).required(),
     eabcDBUser: Joi.string().max(40).required(),
     eabcDBPwd: Joi.string().max(256).required(),
-    ecmDBUrl: Joi.string().pattern(/((([A-Za-z]{3,9}:(?:\/\/)?)(?:[\-;:&=\+\$,\w]+@)?[A-Za-z0-9\.\-]+|(?:www\.|[\-;:&=\+\$,\w]+@)[A-Za-z0-9\.\-]+)((?:\/[\+~%\/\.\w\-_]*)?\??(?:[\-\+=&;%@\.\w_]*)#?(?:[\.\!\/\\\w]*))?)/).required(),
+    ecmDBUrl: Joi.string().pattern(urlPattern).required(),
     ecmDBDriver: Joi.string().max(256).required(),
     ecmDBDialect: Joi.string().max(256).required(),
     ecmDBUser: Joi.string().max(256).max(40).required(),
@@ -18,13 +19,13 @@ const createSetting = {
 };
 const updateSetting = {
   body: Joi.object().keys({
-    mongoUrl: Joi.string().pattern(/((([A-Za-z]{3,9}:(?:\/\/)?)(?:[\-;:&=\+\$,\w]+@)?[A-Za-z0-9\.\-]+|(?:www\.|[\-;:&=\+\$,\w]+@)[A-Za-z0-9\.\-]+)((?:\/[\+~%\/\.\w\-_]*)?\??(?:[\-\+=&;%@\.\w_]*)#?(?:[\.\!\/\\\w]*))?)/).required(),
-    eabcDBUrl: Joi.string().pattern(/((([A-Za-z]{3,9}:(?:\/\/)?)(?:[\-;:&=\+\$,\w]+@)?[A-Za-z0-9\.\-]+|(?:www\.|[\-;:&=\+\$,\w]+@)[A-Za-z0-9\.\-]+)((?:\/[\+~%\/\.\w\-_]*)?\??(?:[\-\+=&;%@\.\w_]*)#?(?:[\.\!\/\\\w]*))?)/).required(),
+    mongoUrl: Joi.string().pattern(urlPattern).required(),
+    eabcDBUrl: Joi.string().pattern(urlPattern).required(),
     eabcDBDriver: Joi.string().max(256).required(),
     eabcDBDialect: Joi.string().max(256).required(),
     eabcDBUser: Joi.string().max(40).required(),
     eabcDBPwd: Joi.string().max(256).required(),
-    ecmDBUrl: Joi.string().pattern(/((([A-Za-z]{3,9}:(?:\/\/)?)(?:[\-;:&=\+\$,\w]+@)?[A-Za-z0-9\.\-]+|(?:www\.|[\-;:&=\+\$,\w]+@)[A-Za-z0-9\.\-]+)((?:\/[\+~%\/\.\w\-_]*)?\??(?:[\-\+=&;%@\.\w_]*)#?(?:[\.\!\/\\\w]*))?)/).required(),
+    ecmDBUrl: Joi.string().pattern(urlPattern).required(),
     ecmDBDriver: Joi.string().max(256).required(),
     ecmDBDialect: Joi.string().max(256).required(),
     ecmDBUser: Joi.string().max(256).max(40).required(),
