ef0c6f544a0a52be772de329008ca3603ae3d56a
saud.iqbal@expertflow.com <saud.iqbal@expertflow.com>
2022-06-07 12:52:15 +0500

    fixing test issue

--- Frontend/src/app/admin/route-settings/mrd-tasks/mrd-tasks.component.spec.ts
+++ Frontend/src/app/admin/route-settings/mrd-tasks/mrd-tasks.component.spec.ts
@@ -1,25 +1,26 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { FormBuilder } from '@angular/forms';
+import { MatDialog } from '@angular/material/dialog';
 import { MrdTasksComponent } from './mrd-tasks.component';
 
-describe('MrdTasksComponent', () => {
+describe("MrdTasksComponent", () => {
   let component: MrdTasksComponent;
-  let fixture: ComponentFixture<MrdTasksComponent>;
-
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ MrdTasksComponent ]
-    })
-    .compileComponents();
-  });
+  let endPointService: any;
+  let snackbarService: any;
+  let commonService: any;
+  let fb: FormBuilder;
+  let dialog: MatDialog;
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(MrdTasksComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    component = new MrdTasksComponent(
+      // commonService,
+      // dialog,
+      // endPointService,
+      // fb,
+      // snackbarService
+    );
   });
 
-  it('should create', () => {
+  it("should create mrd tasks component", () => {
     expect(component).toBeTruthy();
   });
 });
