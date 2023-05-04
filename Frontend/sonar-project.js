28a73820589b8512fb521bb1ed82f82ad08ad087
saud.iqbal@expertflow.com <saud.iqbal@expertflow.com>
2023-05-04 20:44:12 +0500

    Merging CI changes

--- Frontend/sonar-project.js
+++
@@ -1,9 +0,0 @@
-const sonarqubeScanner = require('sonarqube-scanner');
-sonarqubeScanner(
-    {
-        serverUrl: 'https://sonarqube.expertflow.com',
-        options: {
-            'sonar.qualitygate.wait': 'true',
-            'sonar.qualitygate.timeout': '1600'
-        }
-    }, () => { });
