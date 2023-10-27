89bfbccdac8ddf628ea2bbe53291c60895ad619b
muhammad.junaid <muhammad.junaid@expertflow.com>
2023-10-27 12:29:02 +0500

    adding username issue on agent attributes page

--- Frontend/src/app/admin/route-settings/users/users.component.ts
+++ Frontend/src/app/admin/route-settings/users/users.component.ts
@@ -87,6 +87,7 @@ export class UsersComponent implements OnInit {
     if (pageNumber) this.p = pageNumber;
 
     this.userForm = this.formBuilder.group({
+      username:[""],
       firstName: [""],
       lastName: [""],
       roles: [""],
@@ -332,6 +333,7 @@ export class UsersComponent implements OnInit {
     if (roleTip && roleTip.length > 5)
       this.rolesTooltip = roleTip.slice(5, roleTip.length);
     this.userForm.patchValue({
+      username: data.keycloakUser.username ? data.keycloakUser.username : "N/A",
       firstName: data.keycloakUser.firstName
         ? data.keycloakUser.firstName
         : "N/A",
