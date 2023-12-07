90f0d148b8ac42610701c7f5d7f2b3c22fdf1522
Raza Ellahi <raza.ellahi@expertflow.com>
2023-12-07 10:03:01 +0000

    Update precision-queue.component.ts

--- Frontend/src/app/admin/route-settings/precision-queue/precision-queue.component.ts
+++ Frontend/src/app/admin/route-settings/precision-queue/precision-queue.component.ts
@@ -104,7 +104,7 @@ export class PrecisionQueueComponent implements OnInit, AfterViewInit {
           Validators.maxLength(50),
         ],
       ],
-      agentSlaDuration: ["", [Validators.pattern("^[0-9]*$"), Validators.max(100), Validators.min(10)]],
+      agentSlaDuration: ["", [Validators.pattern("^[0-9]*$"), Validators.max(600), Validators.min(10)]],
       mrd: ["", [Validators.required]],
       serviceLevelType: [
         1,
